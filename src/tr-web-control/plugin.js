c841e7b0b1d979ad6160f26fe8389566f8ae3d4a
WingGao <wing.gao@live.com>
2024-08-07 22:21:07 +0800

    feat: 添加原始标签转换

--- src/tr-web-control/plugin.js
+++ src/tr-web-control/plugin.js
@@ -8,7 +8,7 @@ system.plugin = {
 				system.openDialogFromTemplate({
 					id: "dialog-system-replaceTracker",
 					options: {
-						title: system.lang.dialog["system-replaceTracker"].title,  
+						title: system.lang.dialog["system-replaceTracker"].title,
 						width: 600,
 						height: 220
 					}
@@ -28,7 +28,7 @@ system.plugin = {
 				system.openDialogFromTemplate({
 					id: "dialog-auto-match-data-folder",
 					options: {
-						title: system.lang.dialog["auto-match-data-folder"].title,  
+						title: system.lang.dialog["auto-match-data-folder"].title,
 						width: 530,
 						height: 280
 					},
@@ -37,6 +37,21 @@ system.plugin = {
 					}
 				});
 				break;
+			case "convert-tags":
+				const wingTags= ['DEL','DEL_FILE','ERR_VERIFY','ERR_PASSKEY','RAPID_115','RAPID_115_FAIL']
+                transmission.torrents.recently.forEach(t=>{
+					if(t.tags != null && t.tags.length > 0){
+						let tagIds = []
+						t.tags.forEach(v=>{
+							let tid = wingTags.indexOf(v)
+							if(tid>=0) tagIds.push(tid)
+						})
+						system.config.labelMaps[t.hashString] = tagIds
+					}
+                })
+				system.saveConfig()
+				alert("转换完成")
+				break
 		}
 	}
 };
